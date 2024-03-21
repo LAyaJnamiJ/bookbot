75ffa8f61c5d6b6f23c05f753a983e2f3da71fc3
LAyaJnamiJ <layajnamij@gmail.com>
2024-03-21 14:11:37 +0800

    added functionality to auto remove whitespace if necessary and isolated the 'opening a book' part to a function

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ def main():
     book_name = input("Which book dost thou seek?\n").lower().strip()
     path = f"books/{book_name}.txt"
     try:
-        with open(path) as f:
+        book_open(path)
+    except FileNotFoundError as e:
+        try:
+            book_name_split = book_name.split()
+            book_name_joined = ''.join(book_name_split)
+            path = f"books/{book_name_joined}.txt"
+            book_open(path)
+        except FileNotFoundError as e:
+            print("such book does not exist")
+        
+def book_open(path):
+    with open(path) as f:
             file_contents = f.read()
             print(file_contents)
-    except FileNotFoundError as e:
-        print("such book does not exist")
 
 main()
